c0d440caeb6865f45798b6c1bafe3d0e7884e718
bopo <ibopo@126.com>
2022-03-08 10:23:53 +0800

    update commit

--- main.py
+++ main.py
@@ -1,56 +1,46 @@
-import time
-import requests
-import hmac
-import hashlib
+import asyncio
 import base64
-import urllib.parse
-import json
+import hashlib
+import hmac
 import os
+import time
+import urllib.parse
+
+import httpx
 
 
-def send_msg(msg, title, ding_secret, dingding_base_url):
-    ding_secret = ding_secret
-    dingding_base_url = dingding_base_url
+async def send_msg(msg, title, secret, dingding_url):
+    """
+    发送钉钉消息
+
+    :param msg: 消息内容
+    :param title: 消息标题
+    :param secret: 钉钉秘钥
+    :param dingding_url: 钉钉 URL
+    """
+
     timestamp = str(round(time.time() * 1000))
-    secret = ding_secret
     secret_enc = secret.encode('utf-8')
-    string_to_sign = '{}\n{}'.format(timestamp, secret)
-    string_to_sign_enc = string_to_sign.encode('utf-8')
-    hmac_code = hmac.new(secret_enc, string_to_sign_enc,
-                         digestmod=hashlib.sha256).digest()
-    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-    headers = {'Content-Type': 'application/json;charset=utf-8'}
-    body = {
-        "msgtype": "markdown",
-        "markdown": {
-            "title": title,
-            "text": msg
-        },
-        "at": {
-            "isAtAll": "true"
-        }
-    }
-    dingding_url = dingding_base_url + "&timestamp=" + timestamp + "&sign=" + sign
-    requests.post(dingding_url, json.dumps(body), headers=headers)
+
+    string_to_sign_enc = f'{timestamp}\n{secret}'.encode('utf-8')
+
+    code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(code))
+    body = {"msgtype": "markdown", "markdown": {"title": title, "text": msg}, "at": {"isAtAll": "true"}}
+
+    async with httpx.AsyncClient() as client:
+        await client.post(dingding_url, json=body, params={'timestamp': timestamp, 'sign': sign})
 
 
 if __name__ == "__main__":
-    dingding_base_url = os.getenv("PLUGIN_DDBASEURL", default="")
     ding_secret = os.getenv("PLUGIN_DDSECRET", default="")
-    drone_build_status = os.getenv("DRONE_BUILD_STATUS",default="failed")
-    if  drone_build_status == "success":
-        try:
-            send_msg("build is success", "drone build",
-                     ding_secret, dingding_base_url)
-            print("msg send success")
-        except Exception as e:
-            print(e)
-            print("msg send failed")
-    else:
-        try:
-            send_msg("build is failed", "drone build",
-                     ding_secret, dingding_base_url)
-            print("msg send success")
-        except Exception as e:
-            print(e)
-            print("msg send failed")
+    dingding_base_url = os.getenv("PLUGIN_DDBASEURL", default="")
+    drone_build_status = os.getenv("DRONE_BUILD_STATUS", default="failed")
+
+    try:
+        message = 'build is success' if drone_build_status == "success" else 'build is failed'
+        asyncio.run(send_msg(message, 'drone build', ding_secret, dingding_base_url))
+        print("msg send success")
+    except Exception as e:
+        print(e)
+        print("msg send failed")
